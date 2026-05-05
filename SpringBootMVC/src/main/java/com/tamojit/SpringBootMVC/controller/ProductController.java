15a67de8a022e5c480a34c033361ab6cb845c06b
Tamojit Das <tamojitdas181007@gmail.com>
2026-05-05 22:57:20 +0530

    GET requests

--- SpringBootMVC/src/main/java/com/tamojit/SpringBootMVC/controller/ProductController.java
+++ SpringBootMVC/src/main/java/com/tamojit/SpringBootMVC/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.tamojit.SpringBootMVC.controller;
 import com.tamojit.SpringBootMVC.model.Product;
 import com.tamojit.SpringBootMVC.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,10 @@ public class ProductController {
     public List<Product> getProducts() {
         System.out.println("getProducts");
         return service.getProducts(); // getting products from Service layer
+    }
+
+    @RequestMapping("/products/{id}") // params
+    public Product getProductById(@PathVariable int id) { // accepting id from path
+        return service.getProductById(id);
     }
 }
--- SpringBootMVC/src/main/java/com/tamojit/SpringBootMVC/service/ProductService.java
+++ SpringBootMVC/src/main/java/com/tamojit/SpringBootMVC/service/ProductService.java
@@ -16,4 +16,11 @@ public class ProductService {
     public List<Product> getProducts() {
         return products; //processing logic in service layer
     }
+
+    public Product getProductById(int id) {
+        return products.stream() // utilizing Java Stream API to fetch product by id
+                .filter(p -> p.getProdId() == id)
+                .findFirst()
+                .orElse(new Product(100, "Default", 0)); // default result
+    }
 }
